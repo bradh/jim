b0aea6fdf36bbc78e73996580e889502cd9aa556
Brad Hards <bradh@frogmouth.net>
2021-10-07 22:56:46 +1100

    npif: add Radar Parameters data table parsing

--- npif/src/main/java/com/sigmabravo/rnd/jim/npif/tables/sensorparametric/RadarParametersDataTable.java
+++ npif/src/main/java/com/sigmabravo/rnd/jim/npif/tables/sensorparametric/RadarParametersDataTable.java
@@ -118,6 +118,10 @@ public class RadarParametersDataTable extends DataTable {
         return azimuthBeamwidth;
     }
 
+    public double getAzimuthBeamwidthDegrees() {
+        return radiansToDegrees(getAzimuthBeamwidth());
+    }
+
     public void setAzimuthBeamwidth(double azimuthBeamwidth) {
         this.azimuthBeamwidth = azimuthBeamwidth;
     }
@@ -148,6 +152,33 @@ public class RadarParametersDataTable extends DataTable {
 
     public int getRadarMode() {
         return radarMode;
+    }
+
+    public String getRadarModeAsText() {
+        switch (getRadarMode()) {
+            case 0x00:
+                return "Swath";
+            case 0x01:
+                return "Spotlight";
+            case 0x02:
+                return "Real beam ground map";
+            case 0x03:
+                return "Enhanced real beam ground map";
+            case 0x04:
+                return "Doppler beam sharpened map";
+            case 0x05:
+                return "SAR overlaid with MTI";
+            case 0x06:
+                return "Interferometric SAR";
+            case 0x07:
+                return "Polarimetric SAR";
+            case 0x08:
+                return "Inverse SAR";
+            case 0x09:
+                return "Side-looking Array SAR (SLAR)";
+            default:
+                return "Unknown radar mode (" + getRadarMode() + ")";
+        }
     }
 
     public void setRadarMode(int radarMode) {
@@ -190,12 +221,48 @@ public class RadarParametersDataTable extends DataTable {
         return interpulseModulationType;
     }
 
+    public String getInterpulseModulationTypeAsText() {
+        switch (getInterpulseModulationType()) {
+            case 0x00:
+                return "None";
+            case 0x01:
+                return "Chirp";
+            case 0x02:
+                return "Binary phase code – Barker";
+            case 0x03:
+                return "Binary phase code – Galois";
+            case 0x04:
+                return "Quadrature phase code";
+            default:
+                return "Unknown interpulse modulation type (" + getInterpulseModulationType() + ")";
+        }
+    }
+
     public void setInterpulseModulationType(int interpulseModulationType) {
         this.interpulseModulationType = interpulseModulationType;
     }
 
     public int getPulseToPulseModulationType() {
         return pulseToPulseModulationType;
+    }
+
+    public String getPulseToPulseModulationTypeAsText() {
+        switch (getPulseToPulseModulationType()) {
+            case 0x00:
+                return "None";
+            case 0x01:
+                return "Linear step";
+            case 0x02:
+                return "Pseudo-random step";
+            case 0x03:
+                return "Pseudo-random";
+            case 0x04:
+                return "Step plus pseudo-random";
+            default:
+                return "Unknown pulse-to-pulse modulation type ("
+                        + getPulseToPulseModulationType()
+                        + ")";
+        }
     }
 
     public void setPulseToPulseModulationType(int pulseToPulseModulationType) {
@@ -206,12 +273,56 @@ public class RadarParametersDataTable extends DataTable {
         return rangeCompressionProcessingAlgorithm;
     }
 
+    public String getRangeCompressionProcessingAlgorithmAsText() {
+        switch (getRangeCompressionProcessingAlgorithm()) {
+            case 0x00:
+                return "None";
+            case 0x01:
+                return "Stretch compression";
+            case 0x02:
+                return "Analogue matched filter";
+            case 0x03:
+                return "Digital matched filter";
+            case 0x04:
+                return "Stretch plus matched filter";
+            case 0x05:
+                return "Step plus matched filter";
+            default:
+                return "Unknown range compression processing algorithm ("
+                        + getRangeCompressionProcessingAlgorithm()
+                        + ")";
+        }
+    }
+
     public void setRangeCompressionProcessingAlgorithm(int rangeCompressionProcessingAlgorithm) {
         this.rangeCompressionProcessingAlgorithm = rangeCompressionProcessingAlgorithm;
     }
 
     public int getAzimuthCompressionProcessingAlgorithm() {
         return azimuthCompressionProcessingAlgorithm;
+    }
+
+    public String getAzimuthCompressionProcessingAlgorithmAsText() {
+        switch (getAzimuthCompressionProcessingAlgorithm()) {
+            case 0x00:
+                return "None";
+            case 0x01:
+                return "Enhanced real beam";
+            case 0x02:
+                return "Real beam";
+            case 0x03:
+                return "Doppler";
+            case 0x04:
+                return "Polar format";
+            case 0x05:
+                return "Range migration";
+            case 0x06:
+                return "Chirp scaling";
+            default:
+                return "Unknown azimuth compression processing algorithm ("
+                        + getAzimuthCompressionProcessingAlgorithm()
+                        + ")";
+        }
     }
 
     public void setAzimuthCompressionProcessingAlgorithm(
@@ -223,12 +334,66 @@ public class RadarParametersDataTable extends DataTable {
         return autofocusProcessingAlgorithms;
     }
 
+    public String getAutofocusProcessingAlgorithmsAsText() {
+        switch (getAutofocusProcessingAlgorithms()) {
+            case 0x00:
+                return "None";
+            case 0x01:
+                return "Motion compensation (MOCO) only";
+            case 0x02:
+                return "Phase gradient plus MOCO";
+            case 0x03:
+                return "Phase difference plus MOCO";
+            case 0x04:
+                return "Multilook registration plus MOCO";
+            case 0x05:
+                return "Contrast optimisation plus MOCO";
+            case 0x06:
+                return "Prominent point processing plus MOCO";
+            case 0x07:
+                return "Mapdrift plus MOCO";
+            case 0x08:
+                return "Multiple Aperture Mapdrift plus MOCO";
+            default:
+                return "Unknown autofocus processing algorithms ("
+                        + getAutofocusProcessingAlgorithms()
+                        + ")";
+        }
+    }
+
     public void setAutofocusProcessingAlgorithms(int autofocusProcessingAlgorithms) {
         this.autofocusProcessingAlgorithms = autofocusProcessingAlgorithms;
     }
 
     public int getRangeProcessingWeighting() {
         return rangeProcessingWeighting;
+    }
+
+    public String getRangeProcessingWeightingAsText() {
+        switch (getRangeProcessingWeighting()) {
+            case 0x00:
+                return "Uniform";
+            case 0x01:
+                return "-20dB m = 6 Taylor";
+            case 0x02:
+                return "-25dB m = 12 Taylor";
+            case 0x03:
+                return "-30dB m = 23 Taylor";
+            case 0x04:
+                return "-35dB m = 44 Taylor";
+            case 0x05:
+                return "-40dB m = 81 Taylor";
+            case 0x06:
+                return "-40dB m = 6 Dolph-Chebyshev";
+            case 0x07:
+                return "-50dB m = 6 Dolph-Chebyshev";
+            case 0x08:
+                return "-60dB m = 6 Dolph-Chebyshev";
+            case 0x09:
+                return "Spatially varying apodisation";
+            default:
+                return "Unknown range processing weighting (" + getRangeProcessingWeighting() + ")";
+        }
     }
 
     public void setRangeProcessingWeighting(int rangeProcessingWeighting) {
@@ -239,6 +404,35 @@ public class RadarParametersDataTable extends DataTable {
         return azimuthProcessingWeighting;
     }
 
+    public String getAzimuthProcessingWeightingAsText() {
+        switch (getAzimuthProcessingWeighting()) {
+            case 0x00:
+                return "Uniform";
+            case 0x01:
+                return "-20dB m = 6 Taylor";
+            case 0x02:
+                return "-25dB m = 12 Taylor";
+            case 0x03:
+                return "-30dB m = 23 Taylor";
+            case 0x04:
+                return "-35dB m = 44 Taylor";
+            case 0x05:
+                return "-40dB m = 81 Taylor";
+            case 0x06:
+                return "-40dB m = 6 Dolph-Chebyshev";
+            case 0x07:
+                return "-50dB m = 6 Dolph-Chebyshev";
+            case 0x08:
+                return "-60dB m = 6 Dolph-Chebyshev";
+            case 0x09:
+                return "Spatially varying apodisation";
+            default:
+                return "Unknown azimuth processing weighting ("
+                        + getAzimuthProcessingWeighting()
+                        + ")";
+        }
+    }
+
     public void setAzimuthProcessingWeighting(int azimuthProcessingWeighting) {
         this.azimuthProcessingWeighting = azimuthProcessingWeighting;
     }
@@ -247,12 +441,64 @@ public class RadarParametersDataTable extends DataTable {
         return antennaAzimuthWeighting;
     }
 
+    public String getAntennaAzimuthWeightingAsText() {
+        switch (getAntennaAzimuthWeighting()) {
+            case 0x00:
+                return "Uniform";
+            case 0x01:
+                return "-20dB m = 6 Taylor";
+            case 0x02:
+                return "-25dB m = 12 Taylor";
+            case 0x03:
+                return "-30dB m = 23 Taylor";
+            case 0x04:
+                return "-35dB m = 44 Taylor";
+            case 0x05:
+                return "-40dB m = 81 Taylor";
+            case 0x06:
+                return "-40dB m = 6 Dolph-Chebyshev";
+            case 0x07:
+                return "-50dB m = 6 Dolph-Chebyshev";
+            case 0x08:
+                return "-60dB m = 6 Dolph-Chebyshev";
+            default:
+                return "Unknown antenna azimuth weighting (" + getAntennaAzimuthWeighting() + ")";
+        }
+    }
+
     public void setAntennaAzimuthWeighting(int antennaAzimuthWeighting) {
         this.antennaAzimuthWeighting = antennaAzimuthWeighting;
     }
 
     public int getAntennaElevationWeighting() {
         return antennaElevationWeighting;
+    }
+
+    public String getAntennaElevationWeightingAsText() {
+        switch (getAntennaElevationWeighting()) {
+            case 0x00:
+                return "Uniform";
+            case 0x01:
+                return "-20dB m = 6 Taylor";
+            case 0x02:
+                return "-25dB m = 12 Taylor";
+            case 0x03:
+                return "-30dB m = 23 Taylor";
+            case 0x04:
+                return "-35dB m = 44 Taylor";
+            case 0x05:
+                return "-40dB m = 81 Taylor";
+            case 0x06:
+                return "-40dB m = 6 Dolph-Chebyshev";
+            case 0x07:
+                return "-50dB m = 6 Dolph-Chebyshev";
+            case 0x08:
+                return "-60dB m = 6 Dolph-Chebyshev";
+            default:
+                return "Unknown antenna elevation weighting ("
+                        + getAntennaElevationWeighting()
+                        + ")";
+        }
     }
 
     public void setAntennaElevationWeighting(int antennaElevationWeighting) {
