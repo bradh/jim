5cbc43867924707b660dcf326ade7573384acdf5
Brad Hards <bradh@frogmouth.net>
2022-01-01 20:38:18 +1100

    fix serialisation of B16 encoded float

--- s4607/src/main/java/net/frogmouth/rnd/jim/s4607/DwellSegment/DwellSegmentSerialiser.java
+++ s4607/src/main/java/net/frogmouth/rnd/jim/s4607/DwellSegment/DwellSegmentSerialiser.java
@@ -1,5 +1,6 @@
 package net.frogmouth.rnd.jim.s4607.DwellSegment;
 
+import java.io.ByteArrayOutputStream;
 import net.frogmouth.rnd.jim.s4607.Segment.AbstractSegmentSerialiser;
 import net.frogmouth.rnd.jim.s4607.Segment.Segment;
 import net.frogmouth.rnd.jim.s4607.Segment.SegmentType;
@@ -13,7 +14,133 @@ public class DwellSegmentSerialiser extends AbstractSegmentSerialiser {
 
     @Override
     public byte[] serialise(Segment segment) {
-        // TODO: proper implementation
-        return new byte[75];
+        DwellSegment dwell = (DwellSegment) segment;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        baos.writeBytes(getExistenceMask(dwell));
+        if (dwell.getRevisitIndex() != null) {
+            baos.writeBytes(writeI16(dwell.getRevisitIndex()));
+        }
+        if (dwell.getDwellIndex() != null) {
+            baos.writeBytes(writeI16(dwell.getDwellIndex()));
+        }
+        if (dwell.getLastDwellOfRevisit() != null) {
+            baos.writeBytes(writeFL8(dwell.getLastDwellOfRevisit()));
+        }
+        if (dwell.getTargetReportCount() != null) {
+            baos.writeBytes(writeI16(dwell.getTargetReportCount()));
+        }
+        if (dwell.getDwellTime() != null) {
+            baos.writeBytes(writeI32(dwell.getDwellTime()));
+        }
+        baos.writeBytes(writeSA32(dwell.getSensorLatitude()));
+        baos.writeBytes(writeBA32(dwell.getSensorLongitude()));
+        baos.writeBytes(writeS32(dwell.getSensorAltitude()));
+        if (dwell.getScaleFactorLatScale() != null) {
+            baos.writeBytes(writeSA32(dwell.getScaleFactorLatScale()));
+        }
+        if (dwell.getScaleFactorLonScale() != null) {
+            baos.writeBytes(writeBA32(dwell.getScaleFactorLonScale()));
+        }
+        if (dwell.getSensorPositionUncertaintyAlongTrack() != null) {
+            baos.writeBytes(writeI32(dwell.getSensorPositionUncertaintyAlongTrack()));
+        }
+        if (dwell.getSensorPositionUncertaintyCrossTrack() != null) {
+            baos.writeBytes(writeI32(dwell.getSensorPositionUncertaintyCrossTrack()));
+        }
+        if (dwell.getSensorPositionUncertaintyAltitude() != null) {
+            baos.writeBytes(writeI16(dwell.getSensorPositionUncertaintyAltitude()));
+        }
+        if (dwell.getSensorTrack() != null) {
+            baos.writeBytes(writeBA16(dwell.getSensorTrack()));
+        }
+        if (dwell.getSensorSpeed() != null) {
+            baos.writeBytes(writeI32(dwell.getSensorSpeed()));
+        }
+        if (dwell.getSensorVerticalVelocity() != null) {
+            baos.writeBytes(writeS8(dwell.getSensorVerticalVelocity()));
+        }
+        if (dwell.getSensorVerticalVelocityUncertainty() != null) {
+            baos.writeBytes(writeI8(dwell.getSensorVerticalVelocityUncertainty()));
+        }
+        if (dwell.getPlatformOrientationHeading() != null) {
+            baos.writeBytes(writeBA16(dwell.getPlatformOrientationHeading()));
+        }
+        if (dwell.getPlatformOrientationPitch() != null) {
+            baos.writeBytes(writeSA16(dwell.getPlatformOrientationPitch()));
+        }
+        if (dwell.getPlatformOrientationRoll() != null) {
+            baos.writeBytes(writeSA16(dwell.getPlatformOrientationRoll()));
+        }
+        baos.writeBytes(writeSA32(dwell.getDwellCentreLatitude()));
+        baos.writeBytes(writeBA32(dwell.getDwellCentreLongitude()));
+        baos.writeBytes(writeB16(dwell.getDwellAreaRangeHalfExtent()));
+        baos.writeBytes(writeBA16(dwell.getDwellAreaDwellAngleHalfExtent()));
+        if (dwell.getMinimumDetectableVelocity() != null) {
+            baos.writeBytes(writeI8(dwell.getMinimumDetectableVelocity()));
+        }
+        for (Target target : dwell.getTargets()) {
+            if (target.getMtiReportIndex() != null) {
+                baos.writeBytes(writeI16(target.getMtiReportIndex()));
+            }
+            if (target.getTargetLocationLatitude() != null) {
+                baos.writeBytes(writeSA32(target.getTargetLocationLatitude()));
+            }
+            if (target.getTargetLocationLongitude() != null) {
+                baos.writeBytes(writeBA32(target.getTargetLocationLongitude()));
+            }
+            if (target.getTargetLocationDeltaLatitude() != null) {
+                baos.writeBytes(writeS16(target.getTargetLocationDeltaLatitude()));
+            }
+            if (target.getTargetLocationDeltaLongitude() != null) {
+                baos.writeBytes(writeS16(target.getTargetLocationDeltaLongitude()));
+            }
+            if (target.getTargetLocationGeodeticHeight() != null) {
+                baos.writeBytes(writeS16(target.getTargetLocationGeodeticHeight()));
+            }
+            if (target.getTargetVelocityLineOfSightComponent() != null) {
+                baos.writeBytes(writeS16(target.getTargetVelocityLineOfSightComponent()));
+            }
+            if (target.getTargetWrapVelocity() != null) {
+                baos.writeBytes(writeI16(target.getTargetWrapVelocity()));
+            }
+            if (target.getTargetSNR() != null) {
+                baos.writeBytes(writeS8(target.getTargetSNR()));
+            }
+            if (target.getTargetClassification() != null) {
+                baos.writeBytes(writeE8(target.getTargetClassification()));
+            }
+            if (target.getTargetClassificationProbability() != null) {
+                baos.writeBytes(writeI8(target.getTargetClassificationProbability()));
+            }
+            if (target.getTargetMeasurementUncertaintySlantRange() != null) {
+                baos.writeBytes(writeI16(target.getTargetMeasurementUncertaintySlantRange()));
+            }
+            if (target.getTargetMeasurementUncertaintyCrossRange() != null) {
+                baos.writeBytes(writeI16(target.getTargetMeasurementUncertaintyCrossRange()));
+            }
+            if (target.getTargetMeasurementUncertaintyHeight() != null) {
+                baos.writeBytes(writeI8(target.getTargetMeasurementUncertaintyHeight()));
+            }
+            if (target.getTargetMeasurementUncertaintyRadialVelocity() != null) {
+                baos.writeBytes(writeI16(target.getTargetMeasurementUncertaintyRadialVelocity()));
+            }
+            if (target.getTruthTagApplication() != null) {
+                baos.writeBytes(writeI8(target.getTruthTagApplication()));
+            }
+            if (target.getTruthTagEntity() != null) {
+                baos.writeBytes(writeI32(target.getTruthTagEntity()));
+            }
+            if (target.getTargetRadarCrossSection() != null) {
+                baos.writeBytes(writeS8(target.getTargetRadarCrossSection()));
+            }
+        }
+        return baos.toByteArray();
+    }
+
+    private static byte[] getExistenceMask(DwellSegment dwell) {
+        // TODO: build properly
+        return new byte[] {
+            (byte) 0xFF, 0x00, 0x03, (byte) 0xC3, (byte) 0x9f, (byte) 0xFF, 0x00, 0x00
+        };
     }
 }
--- s4607/src/main/java/net/frogmouth/rnd/jim/s4607/Segment/AbstractSegmentSerialiser.java
+++ s4607/src/main/java/net/frogmouth/rnd/jim/s4607/Segment/AbstractSegmentSerialiser.java
@@ -14,6 +14,12 @@ public abstract class AbstractSegmentSerialiser {
         return bb.array();
     }
 
+    protected static byte[] writeI32(long value) {
+        ByteBuffer bb = ByteBuffer.allocate(4);
+        bb.putInt((int) value);
+        return bb.array();
+    }
+
     protected static byte[] writeI16(int value) {
         ByteBuffer bb = ByteBuffer.allocate(2);
         bb.putShort((short) value);
@@ -24,5 +30,68 @@ public abstract class AbstractSegmentSerialiser {
         ByteBuffer bb = ByteBuffer.allocate(1);
         bb.put((byte) value);
         return bb.array();
+    }
+
+    protected static byte[] writeFL8(boolean b) {
+        if (b) {
+            return new byte[] {0x01};
+        } else {
+            return new byte[] {0x00};
+        }
+    }
+
+    protected static byte[] writeBA32(double value) {
+        double ba32 = value * 64.0 / 45.0 * Math.pow(2.0, 32 - 9);
+        int rounded = (int) Math.round(ba32);
+        return writeS32(rounded);
+    }
+
+    protected static byte[] writeBA16(double value) {
+        double ba16 = value * 64.0 / 45.0 * Math.pow(2.0, 16 - 9);
+        int rounded = (int) Math.round(ba16);
+        return writeS16(rounded);
+    }
+
+    protected static byte[] writeSA32(double value) {
+        double sa32 = value * 64.0 / 45.0 * Math.pow(2.0, 32 - 8);
+        int rounded = (int) Math.round(sa32);
+        return writeS32(rounded);
+    }
+
+    protected static byte[] writeSA16(double value) {
+        double sa16 = value * 64.0 / 45.0 * Math.pow(2.0, 16 - 8);
+        int rounded = (int) Math.round(sa16);
+        return writeS16(rounded);
+    }
+
+    protected static byte[] writeS32(int value) {
+        ByteBuffer bb = ByteBuffer.allocate(4);
+        bb.putInt(value);
+        return bb.array();
+    }
+
+    protected static byte[] writeS16(int value) {
+        ByteBuffer bb = ByteBuffer.allocate(2);
+        bb.putShort((short) value);
+        return bb.array();
+    }
+
+    protected static byte[] writeB16(double value) {
+        boolean isNegative = value < 0;
+        double magnitude = Math.abs(value);
+        int intPart = (int) magnitude;
+        double fracPart = magnitude - intPart;
+        int fracInt = (int) (fracPart * Math.pow(2, 7));
+        short res = (short) ((intPart << 7) + fracInt);
+        byte[] enc = writeS16(res);
+        if (isNegative) {
+            enc[0] |= (byte) (0x80);
+        }
+        return enc;
+        // TODO: write tests
+    }
+
+    protected static byte[] writeS8(int value) {
+        return new byte[] {(byte) value};
     }
 }
